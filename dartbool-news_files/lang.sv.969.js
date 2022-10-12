171ebb2c13f2b2a22c5cd7d3a55017c7d7cbc079
vitrinasd <110494656+vitrinasd@users.noreply.github.com>
2022-10-12 20:04:48 +0300

    Add files via upload

---
+++ dartbool-news_files/lang.sv.969.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations=["Blogg","Produkter","Det finns ännu inga blogginlägg. Börja genom att klicka \"Skapa blogginlägg\".","Det finns inga produkter ännu. Börja med att klicka \"Lägg till produkt\".","Det finns inget blogginlägg än. Börja med att lägga till en sida \"Blogg\".","Denna kategori är tom. Lägg till produkter eller välj en annan kategori.","Bildgalleri","Inga bilder hittades i detta bildgalleri","Gratis","Mini","Standard","Pro","Limited","Business"];
